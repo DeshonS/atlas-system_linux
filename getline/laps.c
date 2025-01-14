81fd42aeb1105867af7843b57b7b69256ccb61a3
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2025-01-14 10:46:25 -0600

    changed default indent to tabs

--- getline/laps.c
+++ getline/laps.c
@@ -1,56 +1,56 @@
 #include "laps.h"
 
 typedef struct Car {
-    int id;
-    int laps;
+	int id;
+	int laps;
 } Car;
 
 static Car *cars = NULL;
 static size_t num_cars = 0;
 
 void sort_cars() {
-    for (size_t i = 0; i < num_cars - 1; i++) {
-        for (size_t j = 0; j < num_cars - i - 1; j++) {
-            if (cars[j].id > cars[j + 1].id) {
-                Car temp = cars[j];
-                cars[j] = cars[j + 1];
-                cars[j + 1] = temp;
-            }
-        }
-    }
+	for (size_t i = 0; i < num_cars - 1; i++) {
+		for (size_t j = 0; j < num_cars - i - 1; j++) {
+			if (cars[j].id > cars[j + 1].id) {
+				Car temp = cars[j];
+				cars[j] = cars[j + 1];
+				cars[j + 1] = temp;
+			}
+		}
+	}
 }
 
 void race_state(int *id, size_t size) {
-    if (size == 0) { //frees all memory and resets the race
-        free(cars);
-        cars = NULL;
-        num_cars = 0;
-        return;
-    }
+	if (size == 0) { //frees all memory and resets the race
+		free(cars);
+		cars = NULL;
+		num_cars = 0;
+		return;
+	}
 
-    for (size_t i = 0; i < size; i++) { //car laps
-        int car_exists = 0;
-        for (size_t j = 0; j < num_cars; j++) {
-            if (cars[j].id == id[i]) {
-                cars[j].laps++;
-                car_exists = 1;
-                break;
-            }
-        }
+	for (size_t i = 0; i < size; i++) { //car laps
+		int car_exists = 0;
+		for (size_t j = 0; j < num_cars; j++) {
+			if (cars[j].id == id[i]) {
+				cars[j].laps++;
+				car_exists = 1;
+				break;
+			}
+		}
 
-        if (!car_exists) { //adds new car to the race
-            cars = realloc(cars, (num_cars + 1) * sizeof(Car));
-            cars[num_cars].id = id[i];
-            cars[num_cars].laps = 0;
-            num_cars++;
-            printf("Car %d joined the race\n", id[i]);
-        }
-    }
+		if (!car_exists) { //adds new car to the race
+			cars = realloc(cars, (num_cars + 1) * sizeof(Car));
+			cars[num_cars].id = id[i];
+			cars[num_cars].laps = 0;
+			num_cars++;
+			printf("Car %d joined the race\n", id[i]);
+		}
+	}
 
-    sort_cars();
+	sort_cars();
 
-    printf("Race state:\n"); //shows the current race state
-    for (size_t i = 0; i < num_cars; i++) {
-        printf("Car %d [%d laps]\n", cars[i].id, cars[i].laps);
-    }
+	printf("Race state:\n"); //shows the current race state
+	for (size_t i = 0; i < num_cars; i++) {
+		printf("Car %d [%d laps]\n", cars[i].id, cars[i].laps);
+	}
 }
