1159e96143ae245febab3cb5d380395d78e5a0cb
DeshonS <deshonjewell@gmail.com>
2025-02-26 18:15:51 -0600

    check g

--- readelf/0-hreadelf.c
+++ readelf/0-hreadelf.c
@@ -6,7 +6,6 @@
 #include <arpa/inet.h>
 
 const char* get_os_abi(unsigned char osabi) {
-    static char unknown[20];
     switch (osabi) {
         case ELFOSABI_SYSV: return "UNIX - System V";
         case ELFOSABI_HPUX: return "HP-UX";
@@ -18,9 +17,7 @@ const char* get_os_abi(unsigned char osabi) {
         case ELFOSABI_TRU64: return "TRU64";
         case ELFOSABI_ARM: return "ARM architecture";
         case ELFOSABI_STANDALONE: return "Standalone (embedded)";
-        default: 
-            snprintf(unknown, sizeof(unknown), "<unknown: %x>", osabi);
-            return unknown;
+        default: return "Unknown";
     }
 }
 
@@ -72,20 +69,34 @@ void print_elf_header(const char *filename) {
         exit(EXIT_FAILURE);
     }
 
-    printf("\n--- ELF HEADER DETAILS ---\n");
-    printf("Type: %s\n", get_elf_type(ntohs(ehdr.e_type)));
-    printf("Machine: %s\n", get_machine_type(ntohs(ehdr.e_machine)));
-    printf("Entry point: 0x%x\n", ntohl(ehdr.e_entry));
-    printf("Program header offset: %u\n", ntohl(ehdr.e_phoff));
-    printf("Section header offset: %u\n", ntohl(ehdr.e_shoff));
-    printf("Flags: 0x%x\n", ntohl(ehdr.e_flags));
-    printf("Header size: %u bytes\n", ntohs(ehdr.e_ehsize));
-    printf("Program header entry size: %u bytes\n", ntohs(ehdr.e_phentsize));
-    printf("Number of program headers: %u\n", ntohs(ehdr.e_phnum));
-    printf("Section header entry size: %u bytes\n", ntohs(ehdr.e_shentsize));
-    printf("Number of section headers: %u\n", ntohs(ehdr.e_shnum));
-    printf("Section header string table index: %u\n", ntohs(ehdr.e_shstrndx));
-    printf("-----------------------------\n\n");
+    int is_little_endian = (ehdr.e_ident[EI_DATA] == ELFDATA2LSB);
+    uint16_t (*conv16)(uint16_t) = is_little_endian ? le16toh : be16toh;
+    uint32_t (*conv32)(uint32_t) = is_little_endian ? le32toh : be32toh;
+
+    printf("\nELF Header:\n");
+    printf("  Magic:   ");
+    for (int i = 0; i < EI_NIDENT; i++) {
+        printf("%02x ", ehdr.e_ident[i]);
+    }
+    printf("\n");
+    printf("  Class:                             %s\n", ehdr.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
+    printf("  Data:                              %s\n", is_little_endian ? "2's complement, little endian" : "2's complement, big endian");
+    printf("  Version:                           %d (current)\n", ehdr.e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            %s\n", get_os_abi(ehdr.e_ident[EI_OSABI]));
+    printf("  ABI Version:                       %d\n", ehdr.e_ident[EI_ABIVERSION]);
+    printf("  Type:                              %s\n", get_elf_type(conv16(ehdr.e_type)));
+    printf("  Machine:                           %s\n", get_machine_type(conv16(ehdr.e_machine)));
+    printf("  Version:                           0x%x\n", conv32(ehdr.e_version));
+    printf("  Entry point address:               0x%x\n", conv32(ehdr.e_entry));
+    printf("  Start of program headers:          %u (bytes into file)\n", conv32(ehdr.e_phoff));
+    printf("  Start of section headers:          %u (bytes into file)\n", conv32(ehdr.e_shoff));
+    printf("  Flags:                             0x%x\n", conv32(ehdr.e_flags));
+    printf("  Size of this header:               %u (bytes)\n", conv16(ehdr.e_ehsize));
+    printf("  Size of program headers:           %u (bytes)\n", conv16(ehdr.e_phentsize));
+    printf("  Number of program headers:         %u\n", conv16(ehdr.e_phnum));
+    printf("  Size of section headers:           %u (bytes)\n", conv16(ehdr.e_shentsize));
+    printf("  Number of section headers:         %u\n", conv16(ehdr.e_shnum));
+    printf("  Section header string table index: %u\n", conv16(ehdr.e_shstrndx));
     
     close(fd);
 }
