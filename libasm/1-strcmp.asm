9116f5a0370ba53e708834009d522c38a3db1390
DeshonS <deshonjewell@gmail.com>
2025-03-04 13:43:09 -0600

    testing

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -4,12 +4,12 @@ global asm_strcmp
 asm_strcmp:
     xor rax, rax          ; Set RAX (result) to 0
 .next_char:
-    mov al, byte [rdi]    ; Load byte from the first string (rdi points to the address of the first string)
-    mov dl, byte [rsi]    ; Load byte from the second string (rsi points to the address of the second string)
+    mov al, byte [rdi]    ; Load byte from first string (rdi points to the address of the first string)
+    mov dl, byte [rsi]    ; Load byte from second string (rsi points to the address of the second string)
     cmp al, dl            ; Compare the two bytes
     jne .diff             ; If not equal, jump to the diff label
     test al, al           ; Check if we’ve hit '\0'
-    je .done              ; If '\0', the strings are equal (end of string)
+    je .done              ; If '\0', strings are equal (end of string)
     inc rdi               ; Increment the pointer to the first string
     inc rsi               ; Increment the pointer to the second string
     jmp .next_char        ; Repeat the comparison for the next characters
