6a3e704c99a9a7de28b83e4821474ee4a0d92e3e
DeshonS <deshonjewell@gmail.com>
2025-03-04 13:39:18 -0600

    added temp registry to avoid size mismatches

--- libasm/1-strcmp.asm
+++ libasm/1-strcmp.asm
@@ -2,17 +2,19 @@ section .text
 global asm_strcmp
 
 asm_strcmp:
-    xor rax, rax           ; Set RAX (result) to 0
+    xor rax, rax          ; Set RAX (result) to 0
 .next_char:
-    mov al, [rdi]          ; Load byte from first string
-    cmp al, [rsi]          ; Compare with second string
-    jne .diff              ; If not equal, jump to diff
-    test al, al            ; Check if we’ve hit \0
-    je .done               ; If \0, strings are equal
-    inc rdi                ; Advance pointers
+    mov al, [rdi]         ; Load byte from first string
+    mov dl, [rsi]         ; Load byte from second string
+    cmp al, dl            ; Compare bytes
+    jne .diff             ; If not equal, jump to diff
+    test al, al           ; Check if we’ve hit \0
+    je .done              ; If \0, strings are equal
+    inc rdi               ; Advance pointers
     inc rsi
-    jmp .next_char         ; Repeat
+    jmp .next_char        ; Repeat
 .diff:
-    sub rax, [rsi]         ; Return difference
+    sub al, dl            ; Compute difference
+    movsx rax, al         ; Sign-extend AL to RAX
 .done:
     ret
