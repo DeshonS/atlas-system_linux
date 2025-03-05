d4077e127efda8da7a311a2811865b9b94611d5c
DeshonS <deshonjewell@gmail.com>
2025-03-05 14:58:58 -0600

    corrected redundant code

--- libasm/7-puts.asm
+++ libasm/7-puts.asm
@@ -3,14 +3,11 @@ section .text
     extern asm_strlen
 
 asm_puts:
-    mov     rdi, rdi
+    mov     rsi, rdi
     call    asm_strlen
-    
     mov     rdx, rax
     mov     rax, 1
     mov     rdi, 1
-    mov     rsi, rdi
     syscall
-    
     mov     rax, rdx
     ret
