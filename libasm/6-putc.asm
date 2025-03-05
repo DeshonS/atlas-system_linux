72125b233348b8f9394689ae76bd0c2e344c4a12
DeshonS <deshonjewell@gmail.com>
2025-03-05 12:46:04 -0600

    added text

--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -1,11 +1,14 @@
+section .bss
+    buffer resb 1
+
 section .text
     global asm_putc
 
 asm_putc:
+    mov byte [buffer], dil
     mov rax, 1
     mov rdi, 1
-    mov rsi, rsp
-    mov [rsp], dil
+    mov rsi, buffer
     mov rdx, 1
     syscall
     ret
