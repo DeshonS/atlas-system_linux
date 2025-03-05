5276dcfbc36ea98e39a1458a4461162940896252
DeshonS <deshonjewell@gmail.com>
2025-03-05 14:57:32 -0600

    added fix for bytes returned

--- libasm/7-puts.asm
+++ libasm/7-puts.asm
@@ -6,10 +6,11 @@ asm_puts:
     mov     rdi, rdi
     call    asm_strlen
     
+    mov     rdx, rax
     mov     rax, 1
     mov     rdi, 1
     mov     rsi, rdi
-    mov     rdx, rax
     syscall
     
+    mov     rax, rdx
     ret
