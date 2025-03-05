d3d6c300f5ac262c99b9e18fbc9b1b7cb41d36b6
DeshonS <deshonjewell@gmail.com>
2025-03-05 12:39:32 -0600

    completed 4

---
+++ libasm/4-strstr.asm
@@ -0,0 +1,42 @@
+section .text
+    global asm_strstr
+
+asm_strstr:
+    mov rax, rdi
+    test rsi, rsi
+    jz .return_haystack
+    
+.next_start:
+    mov rdx, rax
+    mov rcx, rsi
+
+.loop:
+    mov bl, byte [rcx]
+    test bl, bl
+    jz .found
+    mov al, byte [rdx]
+    test al, al
+    jz .not_found
+    cmp al, bl
+    jne .continue_search
+    inc rdx
+    inc rcx
+    jmp .loop
+
+.continue_search:
+    inc rax
+    mov dl, byte [rax]
+    test dl, dl
+    jnz .next_start
+
+.not_found:
+    xor rax, rax
+    ret
+
+.found:
+    mov rax, rdi
+    ret
+
+.return_haystack:
+    mov rax, rdi
+    ret
--- libasm/libasm.h
+++ libasm/libasm.h
@@ -6,5 +6,6 @@
 size_t asm_strlen(const char *str);
 int asm_strcmp(const char *s1, const char *s2);
 char *asm_strchr(const char *s, int c);
+char *asm_strstr(const char *haystack, const char *needle);
 
 #endif /* LIBASM_H */
