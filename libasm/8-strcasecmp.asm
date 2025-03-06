82af0cedd8781397632a31a168d40e166ee53b6d
DeshonS <deshonjewell@gmail.com>
2025-03-05 20:50:20 -0600

    dealing with segmentation faults

--- libasm/8-strcasecmp.asm
+++ libasm/8-strcasecmp.asm
@@ -2,25 +2,37 @@ section .text
     global asm_strcasecmp
 
 asm_strcasecmp:
+    test    rdi, rdi
+    jz      .null_pointer
+
+    test    rsi, rsi
+    jz      .null_pointer
+
     .loop:
-        mov rdx, byte [rdi]
-        mov rcx, byte [rsi]
-        or     rdx, 0x20
-        or     rcx, 0x20
-        cmp    rdx, rcx
-        jne    .not_equal
-        test   rdx, rdx
-        jz     .equal
-        inc    rdi
-        inc    rsi
-        jmp    .loop
+        movzx   rdx, byte [rdi]
+        movzx   rcx, byte [rsi]
+        or      rdx, 0x20
+        or      rcx, 0x20
+        cmp     rdx, rcx
+        jne     .not_equal
+
+        test    rdx, rdx
+        jz      .equal
+
+        inc     rdi
+        inc     rsi
+        jmp     .loop
 
     .not_equal:
-        sub    rax, rax
-        mov    al, dl
-        sub    al, cl
+        sub     rax, rax
+        mov     al, dl
+        sub     al, cl
         ret
 
     .equal:
-        xor    rax, rax
+        xor     rax, rax
+        ret
+
+    .null_pointer:
+        xor     rax, rax
         ret
