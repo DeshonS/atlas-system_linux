79c3ec10b3e5babe55ce0d2c71ed0c6dd0651eb2
DeshonS <deshonjewell@gmail.com>
2025-03-05 20:42:07 -0600

    added comments

--- libasm/4-strstr.asm
+++ libasm/4-strstr.asm
@@ -2,47 +2,47 @@ section .text
     global asm_strstr
 
 asm_strstr:
-    mov eax, edi           
-    test esi, esi          
-    jz .return_haystack    
+    ; Arguments:
+    ; rdi - haystack (string to search in)
+    ; rsi - needle (substring to find)
 
-    mov cl, byte [esi]     
-    test cl, cl            
-    jz .return_haystack    
+    test rsi, rsi        ; Check if needle is NULL
+    jz .return_haystack  ; If NULL, return haystack
+
+    mov rax, rdi         ; Save haystack start in RAX
 
 .next_start:
-    mov edx, eax           
-    mov ecx, esi           
+    mov rbx, rdi         ; Save current haystack position
+    mov rcx, rsi         ; Reset needle position
 
-.loop:
-    mov bl, byte [ecx]     
-    test bl, bl            
-    jz .found              
-    mov al, byte [edx]     
-    test al, al            
-    jz .not_found          
-    cmp al, bl             
-    jne .continue_search   
-    inc edx                
-    inc ecx                
-    jmp .loop              
+.check:
+    mov al, byte [rbx]   ; Load char from haystack
+    mov dl, byte [rcx]   ; Load char from needle
+    test dl, dl          ; Check if end of needle
+    jz .found            ; If so, return pointer to match
+    test al, al          ; Check if end of haystack
+    jz .not_found        ; If so, return NULL
+    cmp al, dl           ; Compare characters
+    jne .next_haystack   ; If not equal, move to next haystack position
 
-.continue_search:
-    inc eax                
-    mov dl, byte [eax]     
-    test dl, dl            
-    jnz .next_start        
+    inc rbx              ; Move to next char in haystack
+    inc rcx              ; Move to next char in needle
+    jmp .check           ; Continue checking
+
+.next_haystack:
+    inc rdi              ; Move haystack forward
+    mov al, byte [rdi]   ; Load new character
+    test al, al          ; Check if end of haystack
+    jnz .next_start      ; If not, check new position
 
 .not_found:
-    xor eax, eax           
-    ret                    
+    xor rax, rax         ; Return NULL (no match found)
+    ret
 
 .found:
-    mov eax, edi           
-    add eax, edi           
-    sub eax, edx           
-    ret                    
+    mov rax, rdi         ; Set return value to match location
+    ret
 
 .return_haystack:
-    mov eax, edi           
+    mov rax, rdi         ; If needle is empty, return haystack
     ret
