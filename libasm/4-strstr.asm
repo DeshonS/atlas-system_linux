66104063e2b2eb5a7549f7c9dc62399a7058772f
DeshonS <deshonjewell@gmail.com>
2025-03-05 20:30:41 -0600

    implementing possible fix for core dump

--- libasm/4-strstr.asm
+++ libasm/4-strstr.asm
@@ -2,43 +2,47 @@ section .text
     global asm_strstr
 
 asm_strstr:
-    mov rax, rdi
-    mov rbx, rsi
+    mov rax, rdi           
+    test rsi, rsi          
+    jz .return_haystack    
 
-    mov cl, byte [rbx]
-    test cl, cl
-    jz .return_haystack
+    mov cl, byte [rsi]     
+    test cl, cl            
+    jz .return_haystack    
 
 .next_start:
-    mov rdx, rax
-    mov rcx, rbx
+    mov rdx, rax           
+    mov rcx, rsi           
 
 .loop:
-    mov bl, byte [rcx]
-    test bl, bl
-    jz .found
-    mov al, byte [rdx]
-    test al, al
-    jz .not_found
-    cmp al, bl
-    jne .continue_search
-    inc rdx
-    inc rcx
-    jmp .loop
+    mov bl, byte [rcx]     
+    test bl, bl            
+    jz .found              
+    mov al, byte [rdx]     
+    test al, al            
+    jz .not_found          
+    cmp al, bl             
+    jne .continue_search   
+    inc rdx                
+    inc rcx                
+    jmp .loop              
 
 .continue_search:
-    inc rax
-    mov dl, byte [rax]
-    test dl, dl
-    jnz .next_start
+    inc rax                
+    mov dl, byte [rax]     
+    test dl, dl            
+    jnz .next_start        
 
 .not_found:
-    xor rax, rax
-    ret
+    xor rax, rax           
+    ret                    
 
 .found:
-    ret
+    mov rax, rdi
+    add rax, rdi           
+    sub rax, rdx           
+    ret                    
 
 .return_haystack:
-    mov rax, rdi
+    mov rax, rdi           
     ret
