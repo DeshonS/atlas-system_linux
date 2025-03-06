e12bfd7725cc994cbbc6791d72010f70c65ea037
DeshonS <deshonjewell@gmail.com>
2025-03-05 20:19:54 -0600

    fixed formatting

--- libasm/12-strpbrk.asm
+++ libasm/12-strpbrk.asm
@@ -2,30 +2,30 @@ section .text
     global asm_strpbrk
 
 asm_strpbrk:
-    xor rax, rax  
+    xor rax, rax
 
 .loop:
-    mov cl, byte [rdi]  
-    test cl, cl         
-    jz .done            
+    mov cl, byte [rdi]
+    test cl, cl
+    jz .done
 
-    mov rbx, rsi        
+    mov rbx, rsi
 
 .next_char:
-    mov dl, byte [rbx]  
-    test dl, dl         
-    jz .continue_search 
-    cmp cl, dl          
-    je .found           
+    mov dl, byte [rbx]
+    test dl, dl
+    jz .continue_search
+    cmp cl, dl
+    je .found
 
-    inc rbx             
-    jmp .next_char      
+    inc rbx           
+    jmp .next_char
 
 .continue_search:
-    inc rdi             
-    jmp .loop           
+    inc rdi
+    jmp .loop
 
 .found:
-    mov rax, rdi        
+    mov rax, rdi
 .done:
-    ret  
+    ret
