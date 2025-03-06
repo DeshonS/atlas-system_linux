51cba07f85ea51749c26a28a7e4f6b4e94c7a90d
DeshonS <deshonjewell@gmail.com>
2025-03-05 20:40:42 -0600

    reverting

--- libasm/4-strstr.asm
+++ libasm/4-strstr.asm
@@ -2,45 +2,47 @@ section .text
     global asm_strstr
 
 asm_strstr:
-    test rsi, rsi          
+    mov eax, edi           
+    test esi, esi          
     jz .return_haystack    
 
-    movzx ecx, byte [rsi]  
+    mov cl, byte [esi]     
     test cl, cl            
     jz .return_haystack    
 
-    mov rax, rdi           
-
 .next_start:
-    mov rdx, rax           
-    mov r8, rsi            
+    mov edx, eax           
+    mov ecx, esi           
 
 .loop:
-    movzx r9b, byte [r8]   
-    test r9b, r9b          
+    mov bl, byte [ecx]     
+    test bl, bl            
     jz .found              
-    movzx r10b, byte [rdx] 
-    test r10b, r10b        
+    mov al, byte [edx]     
+    test al, al            
     jz .not_found          
-    cmp r10b, r9b          
+    cmp al, bl             
     jne .continue_search   
-    inc rdx                
-    inc r8                 
+    inc edx                
+    inc ecx                
     jmp .loop              
 
 .continue_search:
-    inc rax                
-    movzx r11b, byte [rax] 
-    test r11b, r11b        
+    inc eax                
+    mov dl, byte [eax]     
+    test dl, dl            
     jnz .next_start        
 
 .not_found:
-    xor rax, rax           
+    xor eax, eax           
     ret                    
 
 .found:
+    mov eax, edi           
+    add eax, edi           
+    sub eax, edx           
     ret                    
 
 .return_haystack:
-    mov rax, rdi           
+    mov eax, edi           
     ret
