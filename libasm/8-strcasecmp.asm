24e4e2e70b39fe816461ee6c4a48805162e28f2d
DeshonS <deshonjewell@gmail.com>
2025-03-05 20:43:57 -0600

    trying to fix memory leaks

--- libasm/8-strcasecmp.asm
+++ libasm/8-strcasecmp.asm
@@ -2,27 +2,25 @@ section .text
     global asm_strcasecmp
 
 asm_strcasecmp:
-.loop:
-    mov     al, byte [rdi]      ; Load byte from s1
-    mov     bl, byte [rsi]      ; Load byte from s2
+    .loop:
+        movzx rdx, byte [rdi]
+        movzx rcx, byte [rsi]
+        or     rdx, 0x20
+        or     rcx, 0x20
+        cmp    rdx, rcx
+        jne    .not_equal
+        test   rdx, rdx
+        jz     .equal
+        inc    rdi
+        inc    rsi
+        jmp    .loop
 
-    or      al, 0x20            ; Convert s1 character to lowercase
-    or      bl, 0x20            ; Convert s2 character to lowercase
+    .not_equal:
+        sub    rax, rax
+        mov    al, dl
+        sub    al, cl
+        ret
 
-    cmp     al, bl              ; Compare the lowercase characters
-    jne     .not_equal          ; If not equal, jump to not_equal
-
-    test    al, al              ; Test if the character is null (end of string)
-    jz      .done               ; If both strings are done, we're done
-
-    inc     rdi                 ; Move to next character in s1
-    inc     rsi                 ; Move to next character in s2
-    jmp     .loop               ; Repeat the loop
-
-.not_equal:
-    sub     al, bl              ; Return a difference (al - bl)
-    ret
-
-.done:
-    xor     eax, eax            ; Return 0 if both strings are equal
-    ret
+    .equal:
+        xor    rax, rax
+        ret
