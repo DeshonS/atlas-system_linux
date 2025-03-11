1231950770a232a2e4c56f46ba48678ee011e66b
DeshonS <deshonjewell@gmail.com>
2025-03-10 20:10:16 -0500

    testing

--- libasm/11-strcspn.asm
+++ libasm/11-strcspn.asm
@@ -3,28 +3,21 @@ section .text
 
 asm_strcspn:
     xor rcx, rcx
-
-.loop_s: 
+.loop_s:
     mov al, [rdi + rcx]
     test al, al
     je .done
-
     mov rbx, rsi
-
 .loop_reject:
     mov dl, [rbx]
     test dl, dl
     je .done
-
     cmp al, dl
     je .done
-
     inc rbx
     jmp .loop_reject
-
     inc rcx
     jmp .loop_s
-
 .done:
     mov rax, rcx
     ret
