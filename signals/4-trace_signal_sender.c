9f749ff18e97369cbe493d1a977a618c44baeb19
DeshonS <deshonjewell@gmail.com>
2025-03-19 13:47:10 -0500

    completed 4

---
+++ signals/4-trace_signal_sender.c
@@ -0,0 +1,26 @@
+#include "signals.h"
+
+/**
+ * handler - custom handler
+ * @signum: sender process ID
+ */
+
+void handler(int signum)
+{
+	pid_t sent = getpid();
+	printf("^\nSIGQUIT sent by %d\n", sent);
+}
+
+/**
+ * trace_signal_sender - traces signal to source
+ * Return: 0 on success, -1 on error
+ */
+
+int trace_signal_sender(void)
+{
+	if (signal(SIGQUIT, handler) == SIG_ERR)
+	{
+		return (-1);
+	}
+	return (0);
+}
