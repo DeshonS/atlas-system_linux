69f309184b1aecbff6582a38a6fbe8d8ace23d99
DeshonS <deshonjewell@gmail.com>
2025-04-02 11:44:01 -0500

    completed 1

---
+++ 0x08_CPython/1-python.c
@@ -0,0 +1,27 @@
+#include <python.h>
+
+void print_python_list(PyObject *p);
+
+void print_python_list(PyObject *p)
+{
+	int size, alloc, i;
+	const char *type;
+	PyListObject *list = (PyListObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
+	size = var->ob_size;
+	alloc = list->allocated;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		type = list->ob_item[i]->ob_type->tp_name;
+		printf("Element %d: %s\n", i, type);
+		if (strcmp(type, "bytes") == 0)
+		{
+			print_python_bytes(list->ob_item[i]);
+		}
+	}
+}
