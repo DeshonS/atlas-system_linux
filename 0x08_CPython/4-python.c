5d84ac8bd13c511d6d6a93f2924fb79713cf3859
DeshonS <deshonjewell@gmail.com>
2025-04-03 09:37:12 -0500

    testing

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -4,14 +4,14 @@ void print_python_string(PyObject *p);
 
 void print_python_string(PyObject *p)
 {
-	if (!PyString_Check(p))
+	if (!PyUnicode_Check(p))
 	{
 		printf("[.] string object info\n");
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
-	char value = ((PyStringObject *)p)->tp_name;
-	int size = ((PyStringObject *)p)->ob_size;
+	char value = ((PyUnicodeObject *)p)->tp_name;
+	int size = ((PyUnicodeObject *)p)->ob_size;
 	printf("[.] string object info\n");
 	/*printf("  type: %s\n");*/
 	printf("  length: %d\n" size);
