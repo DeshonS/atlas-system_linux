2ec0e8bf6092c474eb01260bde1b3d94d1309c2f
DeshonS <deshonjewell@gmail.com>
2025-04-03 11:22:04 -0500

    done

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -11,9 +11,9 @@ void print_python_string(PyObject *p)
 		return;
 	}
 	const char *value = PyUnicode_AsUTF8(p);
-	int size = PyUnicode_GET_SIZE(p);
+	size = PyUnicode_GET_SIZE(p);
 	printf("[.] string object info\n");
 	/*printf("  type: %s\n", type);*/
-	printf("  length: %ld\n", size);
-	printf("  value: %d\n", value);
+	printf("  length: %d\n", size);
+	printf("  value: %s\n", value);
 }
