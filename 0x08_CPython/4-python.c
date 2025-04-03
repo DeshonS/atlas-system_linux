012752dcfe494f4bde76c5e35fb926d2e3d67ee2
DeshonS <deshonjewell@gmail.com>
2025-04-03 11:11:49 -0500

    completed

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -10,11 +10,10 @@ void print_python_string(PyObject *p)
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
-	char value = ((PyUnicodeObject *)p)->ob_name;
-	const char *type = list->ob_item->ob_type->tp_name;
-	int size = ((PyUnicodeObject *)p)->ob_size;
+	char value = ((PyUnicodeObject *)p)->AsUTF8;
+	int size = ((PyUnicodeObject *)p)->length;
 	printf("[.] string object info\n");
-	printf("  type: %s\n", type);
+	/*printf("  type: %s\n", type);*/
 	printf("  length: %ld\n", size);
 	printf("  value: %s\n", value);
 }
