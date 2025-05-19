3ce760b82debda6d51db4ab091d748f4b300473c
DeshonS <deshonjewell@gmail.com>
2025-05-19 14:43:39 -0500

    time to flush

--- strace/strace_1.c
+++ strace/strace_1.c
@@ -72,6 +72,7 @@ int main(int argc, char *argv[]) {
 #endif
                 const char *name = get_syscall_name(syscall_nr);
                 printf("%s\n", name);
+                fflush(stdout);
                 in_syscall = 1;
             } else {
                 in_syscall = 0;
