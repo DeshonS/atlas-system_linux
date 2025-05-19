a804032ffada3b8a2b5abeb535af9a7719f92a19
DeshonS <deshonjewell@gmail.com>
2025-05-19 14:38:14 -0500

    attempting 2

--- strace/strace_1.c
+++ strace/strace_1.c
@@ -1,80 +1,85 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/user.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
-#include "syscalls.h" 
+#include "syscalls.h"
 
-#define SYSCALL_MAX 548
-
-const char *get_syscall_name(long syscall_num)
-{
-	for (size_t i = 0; i < sizeof(syscalls_64_g) / sizeof(syscall_t); i++)
-	{
-		if ((long)syscalls_64_g[i].nr == syscall_num)
-			return syscalls_64_g[i].name;
-	}
-	return NULL;
+static const char *get_syscall_name(long syscall_nr) {
+#if defined(__x86_64__)
+    size_t n = sizeof(syscalls_64_g) / sizeof(syscalls_64_g[0]);
+    if (syscall_nr >= 0 && (size_t)syscall_nr < n)
+        return syscalls_64_g[syscall_nr].name;
+#elif defined(__i386__)
+    size_t n = sizeof(syscalls_32_g) / sizeof(syscalls_32_g[0]);
+    if (syscall_nr >= 0 && (size_t)syscall_nr < n)
+        return syscalls_32_g[syscall_nr].name;
+#else
+#error Unsupported architecture
+#endif
+    return "unknown";
 }
 
-int main(int argc, char *argv[])
-{
-	if (argc < 2)
-	{
-		fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
-		return 1;
-	}
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
+        return 1;
+    }
 
-	pid_t child = fork();
-	if (child == -1)
-	{
-		perror("fork");
-		return 1;
-	}
+    pid_t child = fork();
+    if (child == -1) {
+        perror("fork");
+        return 1;
+    }
 
-	if (child == 0)
-	{
-		ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-		kill(getpid(), SIGSTOP);
-		execv(argv[1], &argv[1]);
-		perror("execv");
-		exit(1);
-	}
-	else
-	{
-		int status;
-		waitpid(child, &status, 0);
+    if (child == 0) {
+        if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1) {
+            perror("ptrace TRACEME");
+            exit(1);
+        }
+        kill(getpid(), SIGSTOP);
+        execv(argv[1], &argv[1]);
+        perror("execv");
+        exit(1);
+    } else {
+        int status;
+        int in_syscall = 0;
+        struct user_regs_struct regs;
 
-		ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+        waitpid(child, &status, 0);
+        ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 
-		while (1)
-		{
-			waitpid(child, &status, 0);
-			if (WIFEXITED(status))
-				break;
+        waitpid(child, &status, 0);
+        ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 
-			struct user_regs_struct regs;
-			ptrace(PTRACE_GETREGS, child, NULL, &regs);
+        while (1) {
+            waitpid(child, &status, 0);
+            if (WIFEXITED(status))
+                break;
 
-			const char *name = get_syscall_name(regs.orig_rax);
-			if (name) {
-				printf("%s\n", name);
-				fflush(stdout);
-			} else {
-				printf("unknown_syscall_%lld\n", regs.orig_rax);
-				fflush(stdout);
-			}
-			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+            ptrace(PTRACE_GETREGS, child, NULL, &regs);
 
-			waitpid(child, &status, 0);
-			if (WIFEXITED(status))
-				break;
-			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
-		}
-	}
+            if (in_syscall == 0) {
+#if defined(__x86_64__)
+                long syscall_nr = regs.orig_rax;
+#elif defined(__i386__)
+                long syscall_nr = regs.orig_eax;
+#else
+#error Unsupported architecture
+#endif
+                const char *name = get_syscall_name(syscall_nr);
+                fprintf(stderr, "%s\n", name);
+                in_syscall = 1;
+            } else {
+                in_syscall = 0;
+            }
 
-	return 0;
+            ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+        }
+    }
+
+    return 0;
 }
