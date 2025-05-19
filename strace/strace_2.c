94a650aad53708924d1a4e13c9b1d5fa8869cf4d
DeshonS <deshonjewell@gmail.com>
2025-05-19 14:47:36 -0500

    attempting strace2

---
+++ strace/strace_2.c
@@ -0,0 +1,64 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/user.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/syscall.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s command [args...]\n", argv[0]);
+        return 1;
+    }
+
+    pid_t child = fork();
+    if (child == -1) {
+        perror("fork");
+        return 1;
+    }
+
+    if (child == 0) {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        kill(getpid(), SIGSTOP);
+        execv(argv[1], &argv[1]);
+        perror("execv");
+        exit(1);
+    } else {
+        int status;
+        struct user_regs_struct regs;
+        int in_syscall = 0;
+
+        waitpid(child, &status, 0);
+        ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+
+        while (1) {
+            waitpid(child, &status, 0);
+            if (WIFEXITED(status))
+                break;
+
+            ptrace(PTRACE_GETREGS, child, NULL, &regs);
+
+            if (in_syscall == 0) {
+                in_syscall = 1;
+                printf("%lld", (long long)regs.orig_rax);
+                fflush(stdout);
+            } else {
+                in_syscall = 0;
+                if (regs.orig_rax == SYS_exit_group)
+                    printf(" = ?\n");
+                else
+                    printf(" = %#llx\n", (long long)regs.rax);
+            }
+
+            ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+        }
+    }
+
+    return 0;
+}
