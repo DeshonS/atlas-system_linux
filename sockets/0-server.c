1353d4960beab939edcd84b192ccd432520f3b2a
DeshonS <deshonjewell@gmail.com>
2025-06-25 07:54:17 -0500

    fixed formatting error

--- sockets/0-server.c
+++ sockets/0-server.c
@@ -7,46 +7,46 @@
 
 int main(void)
 {
-    int server_fd;
-    struct sockaddr_in address;
-    int opt = 1;
+	int server_fd;
+	struct sockaddr_in address;
+	int opt = 1;
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
-    {
-        perror("setsockopt failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+	{
+		perror("setsockopt failed");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(12345);
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(12345);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1)
-    {
-        perror("bind failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1)
+	{
+		perror("bind failed");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
 
-    if (listen(server_fd, 10) == -1)
-    {
-        perror("listen failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
+	if (listen(server_fd, 10) == -1)
+	{
+		perror("listen failed");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
 
-    printf("Server listening on port 12345\n");
+	printf("Server listening on port 12345\n");
 
-    while (1)
-        pause();
+	while (1)
+	pause();
 
-    return 0;
+	return 0;
 }
